94742b6de4dac9b7e92b7fba3b9f3a69474d836b
André Arruda da Silva <adr_hb@hotmail.com>
2025-06-12 05:15:01 -0300

    Created Error Page

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -15,6 +15,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseStatusCodePagesWithRedirects("/StatusCode/{0}");
 
 app.UseHttpsRedirection();
 
