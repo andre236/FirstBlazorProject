bfc0a7c7f70091dfef776443e6357874175e2468
André Arruda da Silva <adr_hb@hotmail.com>
2025-06-16 05:23:42 -0300

    Created files with different extension and apply your functions yet

---
+++ BlazorApp1/Components/Pages/Separation.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BlazorApp1.Components.Pages
+{
+    public partial class Separation
+    {
+        
+        public string Text = "Oi eu sou código c#";
+
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; } = default!;
+
+        public async Task CarregarJS()
+        {
+            var modulo = await JSRuntime
+                .InvokeAsync<IJSObjectReference>("import", "./Components/Pages/Separation.razor.js");
+            //await JSRuntime.InvokeVoidAsync("ShowMessage");
+            await modulo.InvokeVoidAsync("ShowMessageTwo");
+        }
+    }
+
+
+}
---
+++ BlazorApp1/Components/Pages/Separation.razor.js
@@ -0,0 +1,9 @@
+﻿
+alert("Eu sou o Javascript");
+window.ShowMessage = () => {
+    alert("Clicou no botão JS");
+}
+
+export function ShowMessageTwo() {
+    alert('Show Message Two');
+}
