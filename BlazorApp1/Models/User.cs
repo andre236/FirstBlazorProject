9191da0892af6e67f5583d961281b96d1271aee8
André Arruda da Silva <adr_hb@hotmail.com>
2025-06-17 07:59:44 -0300

    Create Validation and Submit the User

---
+++ BlazorApp1/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        [Required(ErrorMessage ="O campo E-mail é obrigatório!")]
+        [EmailAddress(ErrorMessage ="O campo 'E-mail' é inválido.")]
+        public string? Email { get; set; }
+        [Required(ErrorMessage ="O campo 'Senha' é obrigatório!")]
+        [MinLength(8, ErrorMessage ="O campo 'Senha' tem que ter pelo menos 8 caracteres.")]
+        public string? Password { get; set; }
+    }
+}
